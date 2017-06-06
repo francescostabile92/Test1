bd645e60b36fb7b4c834c1dea30445e57d47b3a7
Francesco Stabile <francescostabile@10-21-48-119.0200.sec.it.iosda.org>
2017-06-06 10:45:56 +0200

    ho aggiunto il gruppo

--- Carta d'identita/ViewController.swift
+++ Carta d'identita/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
     var a = ""
     var b = ""
     var istruttori: [Int : String] = [1: "pasquale", 2: "luca", 3:"lelio", 4:"giuseppe"]
+    var gruppo: [Int: String] = [1: "luca", 2: "vincenzo", 3: "antonio", 4: "marina", 5:"bernardo", 6:"francesco"]
     var trovato = false
+    var trovato2 = false
     @IBAction func ilSaluto(_ sender: UIButton){
     a = String(testo.text!)!
     b = a.lowercased().trimmingCharacters(in: CharacterSet.whitespaces)
@@ -24,11 +26,23 @@ class ViewController: UIViewController {
                 trovato = true
             }
         }
+        for i in 1...gruppo.count{
+            if(b==gruppo[i]){
+                trovato2 = true
+            }
+        }
+
             if (trovato == true){
                 saluto.text = "Ciao \(b.capitalized) sei un istruttore fantastico"
                 trovato = false
             }
-            else{
+        else
+        if (trovato2 == true){
+            saluto.text = "Ciao \(b.capitalized) sei un buon"
+            trovato2 = false
+        }
+            else
+            {
                 saluto.text = "Ciao \(b.capitalized) ti auguro una buona giornata all Ios Foundation Program"
             }
         }
